019a546229016f5440da9d0d2b6ed3b90ed18eb6
Tomo83 <tomek.kurzacz@gmail.com>
2017-10-02 01:46:17 +0200

    All files added

---
+++ js/weather.js
@@ -0,0 +1,41 @@
+if('geotion' in navigator) {
+    navigator.geolocation.getCurrentPosition(function(position){
+        loadWeather(position.coords.latitude + ',' + position.coords.longitude);
+    });
+} else {
+    loadWeather("Bytom, PL", "");
+}
+
+$(document).ready(function(){
+    setInterval(getWeather, 10000);
+});
+function loadWeather(location, woeid){
+    $.simpleWeather({
+        location: location,
+        woeid: woeid,
+        unit: 'c',
+        success: function(weather){
+            city = weather.city.toUpperCase() + ', ' + weather.country;
+            temp = weather.temp + '&deg;<span>C</span>';
+            wcode = '<img class="weathericon" src="images/weathericons/' + weather.code + '.svg">';
+            wind = '<th>' + weather.wind.speed + ' ' + weather.units.speed + '</th>';
+            humidity = '<td>' + weather.humidity + ' %</td>';
+            currentlyTemp = '<p>' + weather.currently + '</p>';
+            sunrise = '<td>' + weather.sunrise + '</td>';
+            sunset = '<td>' + weather.sunset + '</td>';
+
+            $(".location").text(city);
+            $(".temperature").html(temp);
+            $(".climate_bg").html(wcode);
+            $(".currentlyTemp").html(currentlyTemp);
+            $(".windspeed").html(wind);
+            $(".humidity").html(humidity); 
+            $(".sunrise").html(sunrise); 
+            $(".sunset").html(sunset); 
+        },
+
+        error: function(error){
+            $(".error").html('<p>' + error + '</p>');
+        }
+    });
+}
